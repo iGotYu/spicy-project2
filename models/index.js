23ce6cc8c53e00bcf0c65e5cff786952d746c943
Andrew Yu <igotyuu421@gmail.com>
2021-05-03 13:26:11 -0700

    updating

--- models/index.js
+++ models/index.js
@@ -3,12 +3,11 @@ const User = require('./User');
 const Connecter = require('./Connecter');
 const { BelongsTo } = require('sequelize/types');
 
-Pokemon.belongsToMany
 
 User.hasMany(Pokemon, {through: Connecter,
-forignKey: 'pokemon_id'
+foreignKey: 'pokemon_id'
 });
 //pokemon belongs to many users through connector 
 Pokemon.belongsToMany (User, {through: Connecter,
-forignKey: 'user_id'
+foreignKey: 'user_id'
 });
