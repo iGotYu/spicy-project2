3bf074603cee2335adeb7cc633ee487b7fb90d91
Andrew Yu <igotyuu421@gmail.com>
2021-05-03 14:06:11 -0700

    searchRoute

--- controllers/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const homeRoute = require('./homeRoute');
-const dashboardRoutes = require('./dashboardRoutes');
-const graphRoutes = require('./graphRoutes');
-const searchRoutes = require('./searchRoutes');
-
-router.use('./home', homeRoute);
