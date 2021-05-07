6d264ba9eb66b9778c3a5c86a1572872ab97e77a
Trevor Eckberg <trevor.eckberg@gmail.com>
2021-05-07 13:55:29 -0700

    added not loggedin functionality and error catching as well as a boat load of internal comments

--- public/js/quicksearch.js
+++ public/js/quicksearch.js
@@ -1,3 +1,4 @@
+//this script will listen for submissions from the quick search bar, and change the url to the search term to call the GET route
 document.querySelector("#quickSearch").addEventListener("submit", event=> {
 event.preventDefault();
 const searchName = document.querySelector("input[type=search]").value;
